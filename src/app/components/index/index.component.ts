ea5375af0eb68ee3a6f706c4f172807efa8d7886
Karen Tarchb <karen.tarchb@gmail.com>
2023-10-23 23:55:47 -0500

    se movio el carrousel y los pop ups

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -3,6 +3,10 @@ import { MatDialog } from '@angular/material/dialog';
 import { PicPopComponent } from '../pop-ups/pic-pop/pic-pop.component';
 import { PicPopTwComponent } from '../pop-ups/pic-pop-tw/pic-pop-tw.component';
 import { PicPopTrComponent } from '../pop-ups/pic-pop-tr/pic-pop-tr.component';
+import { PicPopFrComponent } from '../pop-ups/pic-pop-fr/pic-pop-fr.component';
+import { PicPopFiComponent } from '../pop-ups/pic-pop-fi/pic-pop-fi.component';
+import { PicPopSiComponent } from '../pop-ups/pic-pop-si/pic-pop-si.component';
+import { PicPopSeComponent } from '../pop-ups/pic-pop-se/pic-pop-se.component';
 
 @Component({
   selector: 'app-index',
@@ -26,6 +30,26 @@ export class IndexComponent {
   }
   openpicTr(){
     const dialogref = this.dialog.open(PicPopTrComponent, {
+      height: '95%',
+      width: '95%'});
+  }
+  openpicFr(){
+    const dialogref = this.dialog.open(PicPopFrComponent, {
+      height: '95%',
+      width: '95%'});
+  }
+  openpicFi(){
+    const dialogref = this.dialog.open(PicPopFiComponent, {
+      height: '95%',
+      width: '95%'});
+  }
+  openpicSi(){
+    const dialogref = this.dialog.open(PicPopSiComponent, {
+      height: '95%',
+      width: '95%'});
+  }
+  openpicSe(){
+    const dialogref = this.dialog.open(PicPopSeComponent, {
       height: '95%',
       width: '95%'});
   }
